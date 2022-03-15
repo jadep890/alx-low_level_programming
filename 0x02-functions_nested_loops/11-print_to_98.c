ff01b5d47b7d2af47eedd66fe094a45b4c1ac244
Jade Pieterse <jadepieterse890@gmail.com>
2022-03-15 08:35:06 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,12 +21,12 @@ void print_to_98(int n)
 		}
 	} else if (n >= 98)
 	{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
