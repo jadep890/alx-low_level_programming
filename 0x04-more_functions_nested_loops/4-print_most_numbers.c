036611ea65e0556ff4b12aaad5f9f89efcd31e1d
jadep890 <99265488+jadep890@users.noreply.github.com>
2022-03-17 17:41:06 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,12 @@
 /**
  * print_most_numbers - print from 1 - 9
  */
+
 void print_most_numbers(void)
 {
 	int i;
- 	for (i = 0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
