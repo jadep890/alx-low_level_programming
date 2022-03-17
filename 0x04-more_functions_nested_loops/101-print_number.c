93c754329d0fc7d813431bc0468c233398e0dd90
jadep890 <99265488+jadep890@users.noreply.github.com>
2022-03-17 18:04:08 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,12 +11,12 @@ unsigned int num = n;
 if (n < 0)
 {
 	_putchar('-');
-	num = -num
+	num = -num;
 }
 
 if (num / 10)
 {
 	print_number(num / 10);
 }
-_putchar((num % 10) + '0');
+	_putchar((num % 10) + '0');
 }
