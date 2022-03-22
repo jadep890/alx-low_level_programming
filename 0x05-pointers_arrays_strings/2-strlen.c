e1ff88c585293025651ab56307277dc8cfff7477
Jade Pieterse <jadepieterse890@gmail.com>
2022-03-22 07:13:50 -0700

    This report, by its very lenght, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
