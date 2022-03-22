7b226711f34abcca03c39ae4255f18ee27388130
Jade Pieterse <jadepieterse890@gmail.com>
2022-03-22 07:28:55 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - write the string with a new line
+ * @str: string
+ */
+
+void _puts(char *str);
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+
+	_putchar('\n');
+}
